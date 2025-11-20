56d372ea5ddf10d6f582929911cc47e17372b101
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-11-20 10:24:33 +0100

    Third Commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,11 @@
 import { Highlighter } from "@/components/ui/highlighter";
 import { RainbowButton } from "./ui/rainbow-button";
-
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <>
       <div className="main flex items-center justify-center h-full w-full min-h-[70vh] px-4 sm:px-6 lg:px-8bg-transparent transition-colors duration-300">
-
         <div className="heading text-center max-w-5xl mx-auto">
           <div className="head">
             <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-black dark:text-white">
@@ -25,19 +24,27 @@ const Hero = () => {
                 </span>
               </h1>
               <div className="btn mt-6 sm:mt-8 md:mt-10 flex flex-col sm:flex-row items-center justify-center gap-3 sm:gap-4">
-                <RainbowButton
-                  variant="outline"
-                  size="lg"
-                  className="text-base sm:text-lg md:text-xl w-full sm:w-auto px-6 sm:px-8 hover:shadow-2xl dark:hover:shadow-blue-500/20 transition-shadow duration-300"
+                <Link
+                
+                href="./work">
+                  <RainbowButton
+                    variant="outline"
+                    size="lg"
+                    className="text-base sm:text-lg md:text-xl w-full sm:w-auto px-6 sm:px-8 hover:shadow-2xl dark:hover:shadow-blue-500/20 transition-shadow duration-300"
+                  >
+                    View My Work
+                  </RainbowButton>
+                </Link>
+                <Link
+                href="./contact"
                 >
-                  View My Work
-                </RainbowButton>
-                <RainbowButton
-                  size="lg"
-                  className="text-base sm:text-lg md:text-xl w-full sm:w-auto px-6 sm:px-8 hover:shadow-2xl dark:hover:shadow-purple-500/20 transition-shadow duration-300"
-                >
-                  Get In Touch{" "}
-                </RainbowButton>
+                  <RainbowButton
+                    size="lg"
+                    className="text-base sm:text-lg md:text-xl w-full sm:w-auto px-6 sm:px-8 hover:shadow-2xl dark:hover:shadow-purple-500/20 transition-shadow duration-300"
+                  >
+                    Get In Touch{" "}
+                  </RainbowButton>
+                </Link>
               </div>
             </div>
           </div>
